67ff146b92ddbda1fe21f49a4396183e7d635cd4
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2024-03-23 17:19:13 +0530

    user login and signup create

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import authRoute from "./routes/auth.js";
+import authRoute from "./routes/auth-route.js"
 // import usersRoute from "./routes/users.js";
 // import hotelsRoute from "./routes/hotels.js";
 // import roomsRoute from "./routes/rooms.js";
@@ -29,7 +29,7 @@ app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
 
-// app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute);
 // app.use("/api/users", usersRoute);
 // app.use("/api/hotels", hotelsRoute);
 // app.use("/api/rooms", roomsRoute);
